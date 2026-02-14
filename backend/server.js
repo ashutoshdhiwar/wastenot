0e9938d2a617ef8dd545c092840aadc513aed7c1
ashutoshdhiwar <ashutoshdhiwar04@gmail.com>
2026-02-14 21:00:59 +0530

    vercel deploy setup

--- backend/server.js
+++ backend/server.js
@@ -79,5 +79,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(FRONTEND, 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
